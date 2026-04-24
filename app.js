674c95b5148296947d189f440d6fc57a5883be0d
John <softicaitsolutions@gmail.com>
2026-04-25 00:29:26 +0300

    fix duplicate firebase init

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ if (!admin.app.length){
 const serviceAccount = 
 JSON.parse(process.env.FIREBASE_CONFIG);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
 }
 
 const db = admin.firestore();
--- firebase.js
+++ firebase.js
@@ -1,15 +1,11 @@
 const admin = require("firebase-admin");
 
-let app;
-
 if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
-  app = admin.initializeApp({
+  admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
-} else {
-  app = admin.app();
 }
 
 module.exports = admin;
